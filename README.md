0ff728382be881d5c721c9985b12a1e14b8d9951
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2024-09-12 21:36:34 +0300

    Roll-a-D6
    added description

---
+++ README.md
@@ -0,0 +1,8 @@
+# Read Me 
+This Python script simulates the rolling of a 6-sided dice using ASCII art to represent the dice faces.
+## Usage
+run " python Roll_D6.py " in terminal
+## Example
+![alt text](image.png)
+## Author
+Furkan Sarsilmaz
--- Roll_D6.py
+++ Roll_D6.py
@@ -1,5 +1,6 @@
 import random
-# This code keeps dice shapes as a list and prints according to the incoming number
+
+# Initial dice representation for number 1
 dice = [
     " ------- ",
     "|       |",
@@ -7,58 +8,75 @@ dice = [
     "|       |",
     " ------- ",
 ]
-# Incoming number
-number = random.randint(1,6)
-# Dice shapes for each number
-while True:
+
+# Generate a random number between 1 and 6 to simulate the dice roll
+number = random.randint(1, 6)
+
+# Loop flag to control whether the dice will be rolled again
+loop = True
+
+# Start the dice rolling loop
+while loop == True:
+    # Based on the rolled number, update the dice representation
     if number == 1:
+        # No need to update the dice, as it's already set to represent 1
         dice = dice
     elif number == 2:
+        # Update the dice representation for number 2
         dice = [
-        " ------- ",
-        "|o      |",
-        "|       |",
-        "|      o|",
-        " ------- ",
-    ]
+            " ------- ",
+            "|o      |",
+            "|       |",
+            "|      o|",
+            " ------- ",
+        ]
     elif number == 3:
+        # Update the dice representation for number 3
         dice = [
-        " ------- ",
-        "|      o|",
-        "|   o   |",
-        "|o      |",
-        " ------- ",
-    ]
+            " ------- ",
+            "|      o|",
+            "|   o   |",
+            "|o      |",
+            " ------- ",
+        ]
     elif number == 4:
+        # Update the dice representation for number 4
         dice = [
-        " ------- ",
-        "|o     o|",
-        "|       |",
-        "|o     o|",
-        " ------- ",
-    ]
+            " ------- ",
+            "|o     o|",
+            "|       |",
+            "|o     o|",
+            " ------- ",
+        ]
     elif number == 5:
+        # Update the dice representation for number 5
         dice = [
-        " ------- ",
-        "|o     o|",
-        "|   o   |",
-        "|o     o|",
-        " ------- ",
-    ]
+            " ------- ",
+            "|o     o|",
+            "|   o   |",
+            "|o     o|",
+            " ------- ",
+        ]
     elif number == 6:
+        # Update the dice representation for number 6
         dice = [
-        " ------- ",
-        "|o     o|",
-        "|o     o|",
-        "|o     o|",
-        " ------- ",
-    ]
-    # printing dice with for loop
+            " ------- ",
+            "|o     o|",
+            "|o     o|",
+            "|o     o|",
+            " ------- ",
+        ]
+
+    # Print the current dice representation line by line
     for line in dice:
         print(line)
-    # rolling again or not with if else
-    again = input("Do you want to roll again ? (Y/N) : ")
-    if again == "n":
-        break
+
+    # Prompt the user to ask if they want to roll the dice again
+    again = input("Do you want to roll again? (Y/N): ")
+
+    # If the user chooses 'n', stop the loop; otherwise, roll again
+    if again.lower() == "n":
+        loop = False
     else:
-        number = random.randint(1,6)
+        # Generate a new random number for the next dice roll
+        number = random.randint(1, 6)
